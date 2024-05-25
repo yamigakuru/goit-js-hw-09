9b9d79bbe3712424da5e39d893d215ec4d644148
yamigakuru <qforceplay@gmail.com>
2024-05-25 12:09:18 +0200

    finish

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import glob from 'glob';
 import injectHTML from 'vite-plugin-html-inject';
 import FullReload from 'vite-plugin-full-reload';
+import htmlInject from 'vite-plugin-html-inject';
 
 export default defineConfig(({ command }) => {
   return {
@@ -25,6 +26,6 @@ export default defineConfig(({ command }) => {
       },
       outDir: '../dist',
     },
-    plugins: [injectHTML(), FullReload(['./src/**/**.html'])],
+    plugins: [htmlInject(), injectHTML(), FullReload(['./src/**/**.html'])],
   };
 });
